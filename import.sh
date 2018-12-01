1326597f061173a037082a85b484592522128224
Nicholas Pleschko <lordnigi@gmail.com>
2018-12-01 14:07:38 +0100

    fix typos

--- import.sh
+++ import.sh
@@ -6,7 +6,7 @@ DATABASE_DUMP=$2
 if [ -e .env ]; then
     source .env
 else
-    echo "Please set up your .env file before starting your enviornment."
+    echo "Please set up your .env file before starting your environment."
     exit 1
 fi
 
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 if [ -e .env ]; then
     source .env
 else 
-    echo "Please set up your .env file before starting your enviornment."
+    echo "Please set up your .env file before starting your environment."
     exit 1
 fi
 docker-compose up -d 
